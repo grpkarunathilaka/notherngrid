e2260d69aa2785e0bd8bbb2f7789979d2e446020
Developer <developer@example.com>
2026-06-04 18:13:53 +1000

    feat: add Web3Forms and Google Sheet configuration to development environment

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  web3formsAccessKey: 'YOUR_ACCESS_KEY_HERE',
+  web3formsAccessKey: '40d4aa47-e377-452b-be00-7f8741535d95',
   googleSheetId: '2PACX-1vSKxhg7HuX8uk5naQ0ayQPqUxLZop8iOP_VXFFW4RoZh2Eq_yu961MquXBmOAwnSTCrASx2V8xJyp0A'
 };
